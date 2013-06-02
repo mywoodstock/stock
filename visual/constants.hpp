a71ae66cc952edccd0b9cb154379ae65fc77efa5
Abhinav Sarje <asarje@lbl.gov>
2013-06-02 14:48:25 -0700

    visual stuff

---
+++ visual/constants.hpp
@@ -0,0 +1,25 @@
+/***
+  *  Project:
+  *
+  *  File: constants.hpp
+  *  Created: Mar 10, 2013
+  *  Modified: Sun 10 Mar 2013 10:46:27 AM PDT
+  *
+  *  Author: Abhinav Sarje <asarje@lbl.gov>
+  */
+
+#ifndef __WOO_VIS_CONSTANTS_HPP__
+#define __WOO_VIS_CONSTANTS_HPP__
+
+namespace woo {
+namespace vis {
+
+	const double PI_ = 3.14159265358979323846;
+	const double ZERO_LIMIT_ = 1E-9;
+
+} // namespace vis
+} // namespace woo
+
+
+#endif // __WOO_VIS_CONSTANTS_HPP__
+
